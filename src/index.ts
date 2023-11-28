93c9716a222055d921fddd970fdd0313a01c9fc2
webdevyoo <yjk7454@naver.com>
2023-11-28 12:14:45 +0900

    declare하지 않은 JS파일 추가 및 상대경로 모듈 import

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-import {init, exit} from "myPackage";
+// import {init, exit} from "myPackage";
+import {init, exit} from "./myPackage2"; // myPackage2.js를 직접 불러온다. (myPackage2.d.ts가 없을경우 에러발생하지않음)
+
 init({
+  debug: true,
   url: "true"
 })
 exit(1)
